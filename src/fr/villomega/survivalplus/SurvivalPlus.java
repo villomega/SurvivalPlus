62d48e4daf4e2cc75100dc0e9a4013f68f787cda
BuildTools <unconfigured@null.spigotmc.org>
2020-07-31 17:37:42 +0200

    Ajout des recettes

--- src/fr/villomega/survivalplus/SurvivalPlus.java
+++ src/fr/villomega/survivalplus/SurvivalPlus.java
@@ -1,6 +1,9 @@
 package fr.villomega.survivalplus;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class SurvivalPlus extends JavaPlugin {
@@ -13,6 +16,21 @@ public class SurvivalPlus extends JavaPlugin {
         getCommand("share").setExecutor(new CommandShare(this));
         getCommand("homes").setExecutor(new CommandHomes(this));
         getCommand("info").setExecutor(new CommandInfo(this));
+
+        ShapedRecipe selle = new ShapedRecipe(new ItemStack(Material.SADDLE));
+        selle.shape("CCC", "FFF", "DDD");
+        selle.setIngredient('C', Material.LEATHER);
+        selle.setIngredient('F', Material.IRON_INGOT);
+        selle.setIngredient('D', Material.DIAMOND);
+        getServer().addRecipe(selle);
+
+        ShapedRecipe etiquette = new ShapedRecipe(new ItemStack(Material.NAME_TAG));
+        etiquette.shape("SPD", "DDD", "DDD");
+        etiquette.setIngredient('P', Material.PAPER);
+        etiquette.setIngredient('S', Material.STRING);
+        etiquette.setIngredient('D', Material.DIAMOND);
+        getServer().addRecipe(etiquette);
+
     }
 
     @Override
